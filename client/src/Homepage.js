e22e9b886f0b4f14b6fbcca60b12be84e4e6df17
Yael Harosh <yaelharosh01@gmail.com>
2023-05-08 10:00:47 -0400

    remove h1 from homepage and edit css

--- client/src/Homepage.js
+++ client/src/Homepage.js
@@ -5,7 +5,6 @@ function Homepage({articles}) {
     const mappedAtricles = articles.map(article => <ArticlePage key={article.id} article={article} />)
     return (
         <div>
-            <h1>Home</h1>
             <div id='article-container'>
             {mappedAtricles}
             </div>
